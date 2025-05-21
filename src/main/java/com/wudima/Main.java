104c47d8fc5187299f1e9389afa00fd348338de5
wudima1988 <wudima1988@gmail.com>
2025-05-21 16:34:01 +0300

    add object user in main

--- src/main/java/com/wudima/Main.java
+++ src/main/java/com/wudima/Main.java
@@ -1,5 +1,7 @@
 package com.wudima;
 
+import com.wudima.Users.User;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -8,6 +10,8 @@ public class Main {
         System.out.printf("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
+
+            User user = new User();
 
             System.out.println("i = " + i);
         }
